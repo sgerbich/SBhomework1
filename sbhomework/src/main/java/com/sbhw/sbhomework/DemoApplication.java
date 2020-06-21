7f1dd77558e3de88f900b0a57fe721b8afa80bdb
unknown <sdgerbich@gmail.com>
2020-06-21 19:50:39 -0400

    base requirements

---
+++ sbhomework/src/main/java/com/sbhw/sbhomework/DemoApplication.java
@@ -0,0 +1,35 @@
+package com.sbhw.sbhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Controller
+	public class HomeController {
+
+    @RequestMapping(value = "/")
+    public String index() {
+		
+		return "index.html";
+		
+
+	}
+	
+	@RequestMapping(value="/new")
+	public String another() {
+    return "anotherpage.html";
+}
+
+}
+
+
+}
